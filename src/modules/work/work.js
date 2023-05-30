5b2174db41f7753db977493740f4b672fb1b624c
smails <maxon2504@gmail.com>
2023-05-30 09:40:10 +0300

    комплексное оснащение адаптив

--- src/modules/work/work.js
+++ src/modules/work/work.js
@@ -1,9 +1,17 @@
 import $ from 'jquery'
 import 'slick-carousel'
 
+const workSlider = $('.work__slider');
 
-$('.work__slider').slick({
+workSlider.slick({
   slidesToShow: 1,
   speed: 300,
   useTransform: true,
 });
+
+$('.work__btn').click(function () {
+  $(this).addClass('active').siblings().removeClass('active');
+  $('.work__box').hide();
+  $('.work__box').eq($(this).index()).fadeIn();
+  workSlider.slick('refresh');
+})
